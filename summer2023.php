1f15b195deacd158f86aed79080103508a7bcbf8
star-school <sps.kyoumu@gmail.com>
2025-06-30 09:02:36 +0900

    Add files via upload

---
+++ summer2023.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>サマースクール 準備ページ</title>
+<meta name="robots" content="noindex,nofollow">
+<meta name="description" content="お知らせ・各種データをダウンロードしていただけるページです。" />
+<meta name="keywords" content="パソコン教室,パソコンスクール,ワード,エクセル,初心者,パソコン資格,パソコン検定,Ｐ検,ＭＯＳ,パソコンインストラクター,英語教室,児童英語" />
+<link rel="shortcut icon" href="/dl/favicon.ico">
+<link rel="stylesheet" type="text/css" href="css/sozailink.css" />
+</head>
+<body>
+
+<!--<div class="back"><a href="https://stapre.jp"><img src="images/icon_back.png" alt="戻る"></a></div>-->
+
+<div class="cwrap">
+
+<div class="contents">
+<!-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: -->
+
+<h1 id="1">キッズプログラミング体験【サマースクール2023】</h1>
+<div class="info">
+<p class="text">タブレットのプログラムは、Safariを使用してダウンロードしてください。<br></p>
+<p class="text">使用アプリ ⇒　<a href="https://apps.apple.com/jp/app/scratchjr/id895485086" target="_blank">Scrach Jr</a></p>
+<p class="tit">作成例／テンプレート ダウンロード</p>
+<div class="icon_wrap">
+    <div class="icon_inside">
+    めいろ(1)
+        <a href="summer2023/めいろ(1).sjr" target="_blank"><img src="images/icon_tmp.png"></a>
+        <a href="summer2023/めいろ(1)完成.sjr" target="_blank"><img src="images/icon_sample.png"></a>
+
+    </div>
+    <div class="icon_inside">
+    めいろ(2)
+        <a href="summer2023/めいろ(2).sjr" target="_blank"><img src="images/icon_tmp.png"></a>
+        <a href="summer2023/めいろ(2)完成.sjr" target="_blank"><img src="images/icon_sample.png"></a>
+	</div>
+</div>
+</div>
+
+    
+</div>
+</div>
+
+</body>
+</html>
